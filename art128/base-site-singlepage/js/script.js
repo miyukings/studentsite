73b2abcf8310f99b4afd0e1f3abd2a73d20c3302
miyuki Sekiguchi <miyukis3@hawaii.edu>
2020-03-28 16:45:41 -1000

    fixed

--- art128/base-site-singlepage/js/script.js
+++ art128/base-site-singlepage/js/script.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // TOGGLE NAV //
 
+    // TOGGLE NAV 
     var mymenubutton = document.querySelector('.menu-button');
-    var mytogglemenu = document.querySelector('.toggle-nav');
+    var mytogglenav = document.querySelector('.toggle-nav');
 
     mymenubutton.addEventListener('click', function () {
-        mytogglemenu.classList.toggle('expanded');
+        mytogglenav.classList.toggle('expanded');
     });
+
+    //STICKY NAV - REMOVE EXPANDED CLASS FOR MOBILE
+    var stickynavlinks = document.querySelectorAll(".sticky nav a");
+    for (var i = 0; i < stickynavlinks.length; i++) {
+        stickynavlinks[i].onclick = function () {
+            mytogglenav.classList.remove("expanded");
+        }
+    };
+
 });
 
